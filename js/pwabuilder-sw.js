ee27543f1e9c79c1aabce969936b316ca5075fc0
Kunal <kunal.sonawane@somaiya.edu>
2019-04-01 23:42:58 +0530

    Modified sw.js

--- js/pwabuilder-sw.js
+++ js/pwabuilder-sw.js
@@ -1,83 +1,61 @@
-const CACHE = "pwabuilder-precache";
-const precacheFiles = [
+const staticAssets = [
     '.',
-    '../',
-    '../index.html'
+    '../index.html',
+    '../css/main.css',
+    '../js/main.js',
+    '../404.html'
 ];
 
-self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
+var mode; //used to keep a check whether we are online at this point of time or offline
 
-  console.log("[PWA Builder] Skip waiting on install");
-  self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Caching pages during install");
-      return cache.addAll(precacheFiles);
-    })
-  );
+// install event is called the very first time we register a servic worker
+self.addEventListener('install', async event => {
+    // #caches are http caches provided by service worker check Application >Cache > Cache Storage
+    const cache = await caches.open('static-def'); // stores the shell part
+    cache.addAll(staticAssets);
 });
 
-// Allow sw to control of current page
-self.addEventListener("activate", function (event) {
-  console.log("[PWA Builder] Claiming clients for current page");
-  event.waitUntil(self.clients.claim());
+// called whenever fetxh function is called in the client side
+self.addEventListener('fetch', event => { 
+    console.log(event);
+    const {request} = event;
+    const url = new URL(request.url);
+    if(mode==false) //check if online or offline
+    event.respondWith(cacheData(request)); // if offline we have to check cache
+    else{
+        if(url.origin === location.origin) { // if the url that we are searching for is in the domain check cache first
+            event.respondWith(cacheData(request));
+        } else {
+            event.respondWith(networkFirst(request)); // for outside urls do not check cache 
+        }
+    }
+
 });
 
-// If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) { 
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fromCache(event.request).then(
-      function (response) {
-        // The response was found in the cache so we responde with it and update the entry
-
-        // This is where we call the server to get the newest version of the
-        // file to use the next time we show view
-        event.waitUntil(
-          fetch(event.request).then(function (response) {
-            return updateCache(event.request, response);
-          })
-        );
-
-        return response;
-      },
-      function () {
-        // The response was not found in the cache so we look for it on the server
-        return fetch(event.request)
-          .then(function (response) {
-            // If request was success, add or update it in the cache
-            event.waitUntil(updateCache(event.request, response.clone()));
-
-            return response;
-          })
-          .catch(function (error) {
-            console.log("[PWA Builder] Network request failed and no cache." + error);
-          });
-      }
-    )
-  );
+self.addEventListener('message', function(event){
+    if(event.data=="offline")
+    mode=false
+    else
+    mode=true
+    console.log("message: "+mode);
 });
 
-function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
-      }
-
-      return matching;
-    });
-  });
+async function cacheData(request) 
+{
+    const cachedResponse = await caches.match(request); // check for request object for a match else returns nothing
+    return cachedResponse || fetch(request); 
 }
 
-function updateCache(request, response) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
+async function networkFirst(request) 
+{
+    const cache = await caches.open('dynamic-def'); // stores the dynamic part of the pwa
+
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone()); // map new request objects to the result object
+        return response;
+    } catch (error){
+        return await cache.match(request); // if theres an error in making network first request it will check the caches
+    }
+
 }
