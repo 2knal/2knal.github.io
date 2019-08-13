718e73d606f7fb0aa520510990f847c5c585414d
Kunal <kunal.sonawane@somaiya.edu>
2019-08-13 23:07:43 +0530

    Please run, my boy

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,10 +1,7 @@
 const staticAssets = [
     './css/main.css',
-    './favicon',
-    './fonts',
-    './img',
-    './_scss',
-    'main.js',
+    './favicon/favicon.ico',
+    './js/main.js',
     './index.html',
     './404.html'
 ];
@@ -16,51 +13,40 @@ self.addEventListener('install', async event => {
     // #caches are http caches provided by service worker check Application >Cache > Cache Storage
     const cache = await caches.open('static-def'); // stores the shell part
     cache.addAll(staticAssets);
-    console.log('installed');
+    console.log('IIIIIIInstalled')
 });
 
-// called whenever fetch function is called in the client side
-self.addEventListener('fetch', event => { 
-    console.log(event, 'event');
+// called whenever fetxh function is called in the client side
+self.addEventListener('fetch', event => {
+    console.log(event);
     const {request} = event;
     const url = new URL(request.url);
-    console.log(mode, 'Something?')
     if(mode==false) //check if online or offline
-    event.respondWith(cacheData(request)); // if offline we have to check cache
+        event.respondWith(cacheData(request)); // if offline we have to check cache
     else{
         if(url.origin === location.origin) { // if the url that we are searching for is in the domain check cache first
-            console.log(location.origin, 'Nice')
             event.respondWith(cacheData(request));
         } else {
-            event.respondWith(networkFirst(request)); // for outside urls do not check cache 
+            event.respondWith(networkFirst(request)); // for outside urls do not check cache
         }
     }
+
 });
 
 self.addEventListener('message', function(event){
     if(event.data=="offline")
-    mode=false
+        mode=false
     else
-    mode=true
+        mode=true
     console.log("message: "+mode);
 });
 
-self.addEventListener('notificationclick', function(event){
-    var notification = event.notification;
-    var primaryKey = notification.data.primaryKey;
-    var action = event.action;
-    notification.close();
-    console.log('notif');
-});
-
-async function cacheData(request) 
-{
+async function cacheData(request) {
     const cachedResponse = await caches.match(request); // check for request object for a match else returns nothing
-    return cachedResponse || fetch(request); 
+    return cachedResponse || fetch(request);
 }
 
-async function networkFirst(request) 
-{
+async function networkFirst(request) {
     const cache = await caches.open('dynamic-def'); // stores the dynamic part of the pwa
 
     try {
@@ -70,5 +56,4 @@ async function networkFirst(request)
     } catch (error){
         return await cache.match(request); // if theres an error in making network first request it will check the caches
     }
-
 }
