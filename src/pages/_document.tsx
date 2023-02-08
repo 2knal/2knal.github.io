fcb98526b66fc4a9c98eba592cb977df6791fd13
Kunal Sonawane <2kunalsonawane@gmail.com>
2023-02-08 22:17:34 +0530

    Change theme and icon for PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />        
         <link href="https://api.fontshare.com/v2/css?f[]=clash-display@500,600&f[]=satoshi@500,400&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" rel="stylesheet"/>
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#6867AC" />
       </Head>
       <body className="bg-purple-100 font-satoshi text-base text-purple-700 w-full h-full dark:bg-purple-800 dark:text-cream-100 selection:bg-purple-100 dark:selection:bg-purple-800">
         <Main />
