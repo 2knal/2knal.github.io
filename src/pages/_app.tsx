57ea3f633b17583af56e722b353d5e31bd0b371b
Kunal Sonawane <2kunalsonawane@gmail.com>
2023-03-07 21:06:21 +0530

    Add id to Script tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,8 +9,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
   return (
     <>
-    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-    <Script strategy="lazyOnload">
+    <Script id="gtag" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+    <Script id="gtag-script" strategy="lazyOnload">
       {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
